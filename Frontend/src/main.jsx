ba8cd144368002ae5324b292e142cf4106aea3f9
ranjithmca17 <ranjithmca007@gmail.com>
2025-02-25 17:57:00 +0530

    new

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import router from './routers/router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import 'remixicon/fonts/remixicon.css'
 import {Provider} from "react-redux";
-import { store } from './redux/store.jsx'
+import { store } from "./redux/Store.jsx";
 createRoot(document.getElementById('root')).render(
  <Provider store={store}>
     <RouterProvider router={router} />
